40eb8cf8dffb1d0e0ef131a4dc9cf48c534a1ff6
Sunny Cletus <scletus@gmail.com>
2020-10-26 07:51:36 +0100

    changes

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 /* eslint-disable operator-linebreak */
-export const serverUrl =
+/* export const serverUrl =
   process.env.NODE_ENV === "production"
     ? "https://campusverve.herokuapp.com"
     : "http://localhost:4000";
@@ -8,3 +8,6 @@ export const wsUrl =
   process.env.NODE_ENV === "production"
     ? "wss://campusverve.herokuapp.com"
     : "ws://localhost:4000";
+ */
+export const serverUrl = "http://cvapp-env.eba-7papejsc.eu-central-1.elasticbeanstalk.com";
+export const wsUrl = "ws://cvapp-env.eba-7papejsc.eu-central-1.elasticbeanstalk.com"
